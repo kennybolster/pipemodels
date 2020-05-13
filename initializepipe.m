4b0201b1b80e73fa03d1485b95f34b4a9879683e
Kenny Bolster <54642100+kennybolster@users.noreply.github.com>
2020-05-13 16:38:22 -0700

    implemented second order pipe model

--- initializepipe.m
+++ initializepipe.m
@@ -1,6 +1,7 @@
 nboxes = 10;
-transport = .1;
+transport = .05;
 input = 10;
-decay = 0.01;
+decay = 0.05;
 
 dist = pipemodel(nboxes, transport, input, decay);
+plot(1:nboxes, dist);
---
+++ pipe_2_order.m
@@ -0,0 +1,33 @@
+close all
+nboxes = 10;
+transport = .1;
+input = 10;
+decay = 1e-1;
+n = 100;
+% initializing the parameters
+
+initdistro(1:nboxes,1) = 1;
+% initial concentrations
+
+diffoutA = sparse(1:nboxes, 1:nboxes, -2 * transport);
+diffinA = sparse([1:nboxes-1 2:nboxes], [2:nboxes 1:nboxes-1], transport);
+% diffusive transport matrices
+b = zeros(nboxes,1);
+b(1) = -input;
+% b matrix for tracer input
+
+for i=1:n
+    decayA = sparse(1:nboxes, 1:nboxes, -decay .* initdistro);
+    % matrix to decay the concentration based on the previous distribution
+    A = decayA + diffoutA + diffinA;
+    initdistro = A\b;
+    plot(1:nboxes, initdistro);
+    xlabel('Distance');
+    ylabel('Concentration');
+%     ylim([0 15]);
+    pause(.1)
+end
+
+ 
+
+
